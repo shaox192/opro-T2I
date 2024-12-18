cd1249f68e82049695bb22137fb99733506ea0bf
shaox192 <70292345+shaox192@users.noreply.github.com>
2024-12-18 15:34:24 -0800

    updated readme

--- run.sh
+++ run.sh
@@ -2,16 +2,6 @@
 
 which python
 
-# python ./opro/optimization/optimize_instructions.py \
-#        --optimizer="gpt-3.5-turbo" \
-#        --scorer="gpt-3.5-turbo" \
-#        --instruction_pos="Q_begin" \
-#        --dataset="gsm8k" \
-#        --task="train" \
-#        --meta_prompt_type="both_instructions_and_exemplars" \
-#        --palm_api_key="<your_palm_api_key>" \
-#        --openai_api_key="<your_openai_api_key>"
-
 AGGREGATE_SCORES=True
 MULTI_MODAL=True
 
@@ -22,11 +12,11 @@ python ./opro/optimization/optimize_instructions_T2I.py \
        --save-dir="." \
        --param-aggregate-scores=$AGGREGATE_SCORES \
        --param-multi-modal=$MULTI_MODAL \
-       --param-subset-size 1 \
+       --param-subset-size 20 \
        --param-num-search-steps 10 \
        --param-num-gen-per-search 3 \
-       --openai_api_key="<>" \
-       > out-agg-${AGGREGATE_SCORES}-multimodal-${MULTI_MODAL}.txt
+       --openai_api_key="" \
+       # > out-agg-${AGGREGATE_SCORES}-multimodal-${MULTI_MODAL}.txt
 
 #        --openai_api_base="<>" \
 
